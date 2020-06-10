541505b597491fe5ca3834d3e10b1c72c5584e9b
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-10 20:28:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
       }
     }
     programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP","C"], :known_for =>"Ruby"}
-    binding.pry
+    programmer_hash
 end
 
 def changing_alan
